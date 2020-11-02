1c3f9864e6b51a9808781dcfc75aab32f2055ba7
winterguard <aliasbonfire@gmail.com>
2020-11-02 14:56:08 +0900

    useMemo

--- ReactPieceCode/Immutable/7.js
+++ ReactPieceCode/Immutable/7.js
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 const SelectFruit = ({ selectdFruit, onChange }) => {
   const [maxPrice, setMaxPrice] = useState(1000);
+  const fruits = useMemo(FRUITS.filter(item => item.price <= maxPrice));
   return (
     <div>
       <Select
-        options={FRUITS.filter(item => item.price <= maxPrice)}
+        options={fruits}
         selected={selectdFruit}
         onChange={onChange}
       />
